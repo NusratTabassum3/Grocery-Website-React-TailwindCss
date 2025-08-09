98c58f0da896337e6d49ce2981c18f02ad09890f
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-08-09 22:06:35 +0600

    Create Navbar

--- src/components/Navbar.jsx/Navbar.jsx
+++ src/components/Navbar.jsx/Navbar.jsx
@@ -1,36 +1,80 @@
 import React from "react";
+import { GoHeartFill } from "react-icons/go";
+import { HiShoppingBag } from "react-icons/hi";
+import { IoSearch } from "react-icons/io5";
 
 function Navbar() {
   return (
     <header>
-      <nav className="flex">
-        <a href="" className="">
-          Gr<span>o</span>cify
+      <nav className="max-w-[1400px] mx-auto px-10 h-[14vh] flex justify-between items-center">
+        {/* Logo  */}
+        <a href="" className="text-3xl font-bold">
+          Gr<span className="text-orange-500 uppercase">o</span>cify
         </a>
 
         {/* Destop Menu */}
-        <ul className="flex">
+        <ul className="flex items-center gap-x-15">
           <li className="">
-            <a href="#" className="">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-orange-500 hover:text-orange-500"
+            >
               Home
             </a>
           </li>
           <li className="">
-            <a href="#" className="">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
               About Us
             </a>
           </li>
+
           <li className="">
-            <a href="#" className="">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
               Process
             </a>
           </li>
           <li className="">
-            <a href="#" className="">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
               Contact Us
             </a>
           </li>
         </ul>
+
+        {/* Nav Action  */}
+
+        <div className="flex items-center gap-x-5">
+          {/* input field  */}
+          <div className="flex p-1 border-2 border-orange-500 rounded-full">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search...."
+              autoComplete="off"
+              className="flex-1 h-[4vh] px-3 focus:outline-none"
+            />
+
+            <button className="bg-gradient-to-b from-orange-400 to-orange-500 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl">
+              <IoSearch />
+            </button>
+          </div>
+
+          <a href="" className="text-zinc-800 text-2xl">
+            <GoHeartFill />
+          </a>
+          <a href="" className="text-zinc-800 text-2xl">
+            <HiShoppingBag />
+          </a>
+        </div>
       </nav>
     </header>
   );
