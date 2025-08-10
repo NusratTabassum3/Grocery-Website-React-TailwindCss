8770063c073972d7af00caf5d2126cf9e5f6bb4f
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-08-11 00:41:16 +0600

    Create Heading Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar.jsx/Navbar";
 import Hero from "../Hero/Hero";
+import Category from "../Category/Category";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Category />
     </div>
   );
 }
