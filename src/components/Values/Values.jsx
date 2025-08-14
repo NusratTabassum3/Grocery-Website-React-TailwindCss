97059372e8802140477ca01372a3e251eee4ae28
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-08-15 00:36:23 +0600

    Complete Values Section

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -1,12 +1,62 @@
 import React from "react";
 import Heading from "../Heading/Heading";
-import { FaHeart, FaLeaf } from "react-icons/fa";
+import { FaHeart, FaLeaf, FaSeedling, FaShieldAlt } from "react-icons/fa";
+import Basket from "../../assets/basket-full-vegetables.png";
 
 const Values = () => {
+  const LeftValues = value.slice(0, 2).map((item) => {
+    return (
+      <div className="flex flex-row-reverse items-center gap-7">
+        <div>
+          <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-orange-400 to-orange-500 w-15 h-15 rounded-full">
+            {item.icon}
+          </span>
+        </div>
+
+        <div className="text-right">
+          <h3 className="text-zinc-800 text-3xl font-bold">{item.title}</h3>
+          <p className="text-zinc-600 mt-2">{item.para}</p>
+        </div>
+      </div>
+    );
+  });
+
+  const RightValues = value.slice(2).map((item) => {
+    return (
+      <div className="flex items-center gap-7">
+        <div>
+          <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-orange-400 to-orange-500 w-15 h-15 rounded-full">
+            {item.icon}
+          </span>
+        </div>
+
+        <div>
+          <h3 className="text-zinc-800 text-3xl font-bold">{item.title}</h3>
+          <p className="text-zinc-600 mt-2">{item.para}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <section>
       <div className="max-w-[1400px] mx-auto px-10">
         <Heading highlight="Our" heading="Value" />
+
+        <div className="flex gap-5 mt-15 ">
+          {/* Left Values  */}
+          <div className="min-h-100 flex flex-col justify-between">
+            {LeftValues}
+          </div>
+          <div className="w-1/2">
+            <img src={Basket} alt="" className="" />
+          </div>
+
+          {/* Right Values   */}
+          <div className="min-h-100 flex flex-col justify-between">
+            {RightValues}
+          </div>
+        </div>
       </div>
     </section>
   );
@@ -26,5 +76,17 @@ const value = [
     title: "Always Fresh",
     para: "It is a long established fact that a reader will be distracted by the readable.",
     icon: <FaLeaf />,
+  },
+  {
+    id: 3,
+    title: "Food Safety",
+    para: "It is a long established fact that a reader will be distracted by the readable.",
+    icon: <FaShieldAlt />,
+  },
+  {
+    id: 4,
+    title: "100% Organic",
+    para: "It is a long established fact that a reader will be distracted by the readable.",
+    icon: <FaSeedling />,
   },
 ];
