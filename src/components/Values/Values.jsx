a6757b7b1b648fb6248b168d92cd4ddab79d389a
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-08-16 00:00:17 +0600

    Make Values Section Responsive

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -6,14 +6,14 @@ import Basket from "../../assets/basket-full-vegetables.png";
 const Values = () => {
   const LeftValues = value.slice(0, 2).map((item) => {
     return (
-      <div className="flex flex-row-reverse items-center gap-7">
+      <div className="flex md:flex-row-reverse items-center gap-7">
         <div>
           <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-orange-400 to-orange-500 w-15 h-15 rounded-full">
             {item.icon}
           </span>
         </div>
 
-        <div className="text-right">
+        <div className="md:text-right">
           <h3 className="text-zinc-800 text-3xl font-bold">{item.title}</h3>
           <p className="text-zinc-600 mt-2">{item.para}</p>
         </div>
@@ -43,17 +43,17 @@ const Values = () => {
       <div className="max-w-[1400px] mx-auto px-10">
         <Heading highlight="Our" heading="Value" />
 
-        <div className="flex gap-5 mt-15 ">
+        <div className="flex md:flex-row flex-col gap-15 md:gap-5 mt-15 ">
           {/* Left Values  */}
-          <div className="min-h-100 flex flex-col justify-between">
+          <div className="md:min-h-100 gap-15 flex flex-col justify-between">
             {LeftValues}
           </div>
-          <div className="w-1/2">
+          <div className="md:flex w-1/2 hidden">
             <img src={Basket} alt="" className="" />
           </div>
 
           {/* Right Values   */}
-          <div className="min-h-100 flex flex-col justify-between">
+          <div className="md:min-h-100 gap-15 flex flex-col justify-between">
             {RightValues}
           </div>
         </div>
