ff313b53eed1eb8d09ba3861970abd840866bc96
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-08-18 01:49:13 +0600

    Create Footer Section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer className="bg-zinc-800 py-20"></footer>;
+};
+
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,7 @@ import Products from "../Products/Products";
 import Discount from "../Discount/Discount";
 import Process from "../Process/Process";
 import Testimonials from "../Testimonials/Testimonials";
+import Footer from "../Footer/Footer";
 
 function Home() {
   return (
@@ -19,6 +20,7 @@ function Home() {
       <Discount />
       <Process />
       <Testimonials />
+      <Footer />
     </div>
   );
 }
